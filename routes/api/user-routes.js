45d4e1cc407c883d699148521846c1cc0e469e52
Matthew Damron <mattdamron@msn.com>
2021-02-09 16:15:30 -0700

    hatch password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-const {
-    User
-} = require('../../models');
+const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -56,6 +54,36 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+// POST /api/users/login
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({
+                message: 'No user with that email address!'
+            });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({
+                message: 'Incorrect password!'
+            });
+            return;
+        }
+
+        res.json({
+            user: dbUserData,
+            message: 'You are now logged in!'
+        });
+    });
 });
 
 // PUT /api/users/1
