7fbffb74d2ac84663c10606c92a14f250290d395
Matthew Damron <mattdamron@msn.com>
2021-02-16 07:11:13 -0700

    added handlebars and homepage

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: 3306,
+    // Turn console logging on(true) or off(false)
+    logging: false
   });
 }
 
