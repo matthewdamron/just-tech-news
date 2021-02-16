1e0104544c132ffa13fda4824a9ada03be45c555
Matthew Damron <mattdamron@msn.com>
2021-02-16 13:49:15 -0700

    add login and logout

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+require('dotenv').config();
+
+// Local PORT or Heroku
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -17,6 +21,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 // turn on handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+// Save sessions in cookies
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.COOKIE_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ db: sequelize })
+};
+
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
