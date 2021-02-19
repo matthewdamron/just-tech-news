13101139a2675dc67d50ccd51ed3cddb21325212
Matthew Damron <mattdamron@msn.com>
2021-02-18 17:24:16 -0700

    update code

--- server.js
+++ server.js
@@ -1,45 +1,39 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const path = require('path');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
 
-require('dotenv').config();
-
-// Local PORT or Heroku
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// turn on public
-app.use(express.static(path.join(__dirname, 'public')));
-
-// turn on handlebars
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-// Save sessions in cookies
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.COOKIE_SECRET,
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
-  store: new SequelizeStore({ db: sequelize })
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
 
-// turn on routes
-app.use(routes);
+const helpers = require('./utils/helpers');
 
-// turn on connection to db and server
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening' + ' ' + PORT));
+  app.listen(PORT, () => console.log('Now listening'));
 });
